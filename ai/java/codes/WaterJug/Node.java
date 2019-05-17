f054d84c3f4050db76a13d0128d7c68e2735d0ce
Akshay Dongare <34915737+akshaydongaregit@users.noreply.github.com>
2019-05-17 18:52:48 +0530

    Update Node.java

--- ai/java/codes/WaterJug/Node.java
+++ ai/java/codes/WaterJug/Node.java
@@ -3,7 +3,7 @@ public class Node
 	
 	Node parent = null;
 	int x,y;
-	int depth;
+	int depth=0;
 
 	public Node(int x,int y){
 		this.x = x ;
@@ -27,5 +27,43 @@ public class Node
 	}
 	public Node getParent(){
 		return this.parent;
+    }
+    
+    public boolean isSame(Node node){
+        return ( this.x == node.getX() && this.y == node.getY() ) ;
 	}
+	
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	@Override
+	public String toString(){
+		String toString = "["+x+","+y+"]" ;
+		
+		Node iparent = this.parent ;
+		 while(iparent != null){
+			//System.out.print(".");
+			toString += " <-- ["+iparent.getX()+","+iparent.getY()+"]" ;
+			iparent = iparent.getParent();
+		}
+
+		return toString;
+	}
+
+	public boolean isRepeated(){
+		Node iparent = this.parent ;
+		while(iparent != null){
+			if(this.isSame(iparent))
+				return true;
+			iparent = iparent.getParent();
+		}
+
+		return false;
+	}
+
 }
